597b38ac2ebe2045d577b041018981d913c037e9
Eddie Moon <eddiemoonie@gmail.com>
2018-01-29 17:09:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,4 +25,12 @@ class TicTacToe
   def input_to_index(user_input)
     user_input.to_i - 1
   end
+
+  def position_taken?(@board, index)
+    if @board[index] == " " || @board[index] == "" || @board[index] == nil
+      false
+    elsif @board[index] == "X" || @board[index] == "O"
+      true
+    end
+  end
 end
